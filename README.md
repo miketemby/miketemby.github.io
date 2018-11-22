a831073d60a990a494ebc8c6e1075e4e32819e71
Mike Temby <41090032+miketemby@users.noreply.github.com>
2018-11-23 06:26:39 +1100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+frontend-nanodegree-resaurant-reivew-app
+===============================
+
+# Restaurant Review App
+
+Javascript restaurant review site, built with HTML, CSS and Javascript with offline features.
+* The site has been made responsive and includes offline content for any visited pages 
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+An up to date browser is required to run this locally (Chrome, Safari, Firefox etc), as well as the ability to run an HTTP server using SSL. 
+
+
+### Installing
+
+* Download the zip file
+* Expand to a local directory
+* Start an HTTP server to serve the files
+* Configure your server to use SSL
+* Edit the 'host' and 'port' variables in the dbhelper.js file to match your configuration
+* Launch the site
+
+## Versioning
+
+v 1.0
+
+## Authors
+
+Michael Temby
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+
---
+++ sw.js
@@ -0,0 +1,71 @@
+// on install add assets to cache
+self.addEventListener('install', function(evt) {
+    var urlsToCache = [
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+        // how to add map tiles?
+    ];
+
+    evt.waitUntil(
+       
+        caches.open('assets').then(function(cache) {
+            cache.addAll(urlsToCache);
+        })
+        
+    );
+
+});
+
+// capture fetch events
+self.addEventListener('fetch', function(evt) {
+    
+    //call respond with and return cached assets if they match
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            if(response) {
+                return response;
+            }
+
+            // if they dont match we want to try and fetch the live asset and capture and cache the response
+            // in both the request a response we need to clone it as they can only be consumed once and we dont want to stop the browswer getting the response. 
+            var fetchRequest = evt.request.clone();
+  
+            return fetch(fetchRequest).then(function(response) {
+
+                // Check if we received a valid response and if not just return the response becuase there is nothing to cache
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+  
+                // if we get a valid response, clone it, cache it and return it
+                var responseToCache = response.clone();
+  
+                caches.open('assets').then(function(cache) {
+                    cache.put(evt.request, responseToCache);
+                });
+  
+                return response;
+            });
+
+        })
+    );
+
+});
